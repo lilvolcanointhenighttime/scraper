4c8ccec14f109552b7afc8d4e1aa53c90b224c3a
lilvolcaninthenigthtime <alvicov@mail.ru>
2024-07-01 16:30:55 +0300

    added hh_resume_search

--- src/hhresume.py
+++ src/hhresume.py
@@ -1,27 +1,33 @@
 from bs4 import BeautifulSoup
 import asyncio
 import aiohttp
-
-url = "https://hh.ru/search/resume?page=0"
-# https://hh.ru/search/resume?text=&pos=full_text&logic=normal&exp_period=all_time&page=
-
+from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
 
 
-async def async_request(url: str, headers: dict = {}) -> dict:
-    async with aiohttp.ClientSession() as session:
-        async with session.get(headers=headers, url=url) as response:
-            return await response.text()
+url = "https://hh.ru/search/resume?"
+# https://hh.ru/search/resume?&pos=full_text&logic=normal&exp_period=all_time
+
+
+
+async def async_query(session: aiohttp.ClientSession, headers: dict, url: str, model: BaseModel) -> dict:
+    query_params: dict = model.__dict__
+    for key, value in query_params.items():
+        url += f"&{str(key)}={str(value)}"
+    
+    async with session.get(headers=headers, url=url) as response:
+        data = jsonable_encoder(await response.text())
+        return data
         
-async def get_amount_of_resumes_and_applicatns(url):
-    data = await async_request(url)
+
+async def get_amount_of_resumes_and_applicants(data) -> str:
     soup = BeautifulSoup(data, "lxml")
     block_main = soup.find("div", class_="HH-MainContent HH-Supernova-MainContent")
-    block_amount_of_resumes_and_applicants = block_main.find("h1", class_="bloko-header-section-3")
-    return block_amount_of_resumes_and_applicants
+    amount_of_resumes_and_applicants = block_main.find("h1", class_="bloko-header-section-3").text
+    return amount_of_resumes_and_applicants
 
 
-async def get_resumes():
-    data = await async_request(url)
+async def get_resumes_items(data) -> list:
     soup = BeautifulSoup(data, "lxml")
     block_main = soup.find("div", class_="HH-MainContent HH-Supernova-MainContent")
     all_resumes_resultset = block_main.find_all("div", attrs={"data-qa": "resume-serp__resume"})
@@ -34,7 +40,7 @@ async def get_resumes():
         salary = resume.find_all("div", class_="bloko-text bloko-text_strong")[0].text if resume.find_all("div", class_="bloko-text bloko-text_strong") else ""
         experience = resume.find_all("div", attrs={"data-qa": "resume-serp__resume-excpirience-sum"})[0].text if resume.find_all("div", attrs={"data-qa": "resume-serp__resume-excpirience-sum"}) else ""
         resume_dict = {
-            "link": link,
+            "link": f"https://hh.ru{link}",
             "title": title,
             "age": age,
             "salary": salary,
@@ -42,4 +48,12 @@ async def get_resumes():
         }
         items.append(resume_dict)
     return items
-    
+
+async def get_amount_and_items(data) -> dict:
+    amount_of_resumes_and_applicants: str = await get_amount_of_resumes_and_applicants(data)
+    items: list = await get_resumes_items(data)
+    respons = {
+        "items": items,
+        "found": amount_of_resumes_and_applicants
+    }
+    return respons
--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 import asyncio
-import copy
 from typing import Annotated, List
 import aiohttp
 from fastapi import FastAPI, Header
 from pydantic import BaseModel, Field
 from contextlib import asynccontextmanager
+from .hhresume import *
 
 
 aiohttp_session = None
@@ -18,12 +18,14 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(title="Scraping", lifespan=lifespan)
 
+# Schemas
 class HHVacanciesQueryParamsModel(BaseModel):
     text: str = Field("", description="Переданное значение ищется во всех полях вакансии")
     area: int = Field(113, description="Регион. Необходимо передавать id из справочника /areas. Можно указать несколько значений https://api.hh.ru/openapi/redoc#tag/Obshie-spravochniki/operation/get-areas")
     only_with_salaru: bool = Field(False, description="Показывать вакансии только с указанием зарплаты")
     per_page: int = Field(10, description="Количество элементов на странице")
     page: int = Field(0, description="Текущая страница")
+    order_by: str = Field("", description="Сортировка списка вакансий. Справочник с возможными значениями: vacancy_search_order в https://api.hh.ru/openapi/redoc#tag/Obshie-spravochniki/operation/get-dictionaries. Если выбрана сортировка по удалённости от гео-точки distance, необходимо также задать её координаты: sort_point_lat, sort_point_lng")
 
 class HHVacanciesResponseModel(BaseModel):
     items: List[dict] = Field(..., description="Список с вакинсиями")
@@ -39,52 +41,63 @@ class HHVacanciesResponseModel(BaseModel):
 
 class HHResumesQueryParamsModel(BaseModel):
     text: str = Field("", description="Переданное значение ищется во всех полях резюме")
+    currency: str = Field("", description="Код валюты. Возможные значения перечислены в поле currency.code в https://api.hh.ru/openapi/redoc#tag/Obshie-spravochniki/operation/get-dictionaries")
+    salary_from: int = Field(..., description="Нижняя граница желаемой заработной платы (ЗП). По умолчанию в выдачу добавляются также резюме с неуказанной ЗП. Для выдачи резюме только с указанной ЗП передайте параметр label=only_with_salary")
+    salary_to: int = Field(..., description="Верхняя граница желаемой заработной платы (ЗП). По умолчанию в выдачу добавляются также резюме с неуказанной ЗП. Для выдачи резюме только с указанной ЗП передайте параметр label=only_with_salary")
+    area: int = Field(113, description="Регион. Необходимо передавать id из справочника /areas. Можно указать несколько значений https://api.hh.ru/openapi/redoc#tag/Obshie-spravochniki/operation/get-areas")
+    label: str = Field("", description="Дополнительный фильтр. Возможные значения перечислены в поле resume_search_label в https://api.hh.ru/openapi/redoc#tag/Obshie-spravochniki/operation/get-dictionaries. Можно указать несколько значений")
+    age_from: int = Field(..., description="Нижняя граница возраста соискателя в годах. По умолчанию в выдачу добавляются также резюме с неуказанным возрастом. Для выдачи резюме только с указанным возрастом передайте значение only_with_age в параметре label")
+    age_to: int = Field(..., description="Верхняя граница возраста соискателя в годах. По умолчанию в выдачу добавляются также резюме с неуказанным возрастом. Для выдачи резюме только с указанным возрастом передайте значение only_with_age в параметре label")
+    relocation: str = Field("", description="Готовность к переезду. Возможные значения указаны в поле resume_search_relocation в https://api.hh.ru/openapi/redoc#tag/Obshie-spravochniki/operation/get-dictionaries. Необходимо указывать вместе с параметром area")
+    period: int = Field(..., description="Поиск ведется по резюме, опубликованным за указанный период в днях. Если период не указан, поиск ведется без ограничений по дате публикации")
+    education_level: str = Field("", description="Уровень образования. Возможные значения перечислены в поле education_level в https://api.hh.ru/openapi/redoc#tag/Obshie-spravochniki/operation/get-dictionaries. Если параметр не указан, поиск ведется без ограничений на уровень образования")
+    employment: str = Field("", description="Тип занятости. Возможные значения перечислены в поле employment в https://api.hh.ru/openapi/redoc#tag/Obshie-spravochniki/operation/get-dictionaries. Можно указать несколько значений")
+    skill: str = Field("", description="Ключевые навыки. Указывается один или несколько идентификаторов ключевых навыков. Значения можно получить из поля id в https://api.hh.ru/openapi/redoc#tag/Podskazki/operation/get-skill-set-suggests")
+    gender: str = Field("", description="Пол соискателя. Возможные значения перечислены в поле gender в https://api.hh.ru/openapi/redoc#tag/Obshie-spravochniki/operation/get-dictionaries. По умолчанию вне зависимости от значения параметра будут найдены резюме, в которых пол не указан, исключить из поисковой выдачи такие резюме можно с помощью параметра label=only_with_gender")
+    language: str = Field("", description="Знание языка. Можно указать несколько значений. Возможные значения перечислены в https://api.hh.ru/openapi/redoc#tag/Obshie-spravochniki/operation/get-languages")
+    schedule: str = Field("", description="График работы. Возможные значения перечислены в поле schedule в https://api.hh.ru/openapi/redoc#tag/Obshie-spravochniki/operation/get-dictionaries. Можно указать несколько значений")
+    order_by: str = Field("", description="Сортировка списка резюме. Возможные значения перечислены в поле resume_search_order в https://api.hh.ru/openapi/redoc#tag/Obshie-spravochniki/operation/get-dictionaries")
+    citizenship: str = Field("", description="Страна гражданства соискателя. Возможные значения перечислены в https://github.com/hhru/api/blob/master/docs/areas.md#countries. Можно указать несколько значений")
+    work_ticket: str = Field("", description="Страна, в которой у соискателя есть разрешение на работу. Возможные значения перечислены в https://github.com/hhru/api/blob/master/docs/areas.md#countries. Можно указать несколько значений")
+    page: int = Field(int, description="Текущая страница")
+    per_page: int = Field(int, description="Количество отображаемых вакансий на странице")
 
 class HHResumesResponseModel(BaseModel):
     items: List[dict] = Field(..., description="Список с резюме")
-    found: int = Field(..., description="Количество найденных резюме")
+    found: str = Field(..., description="Количество найденных резюме у соискателей")
 
 
-
+# urls
 hh_vacancies_url = 'https://api.hh.ru/vacancies?clusters=true'
-hh_resume_url = "https://hh.ru/search/resume?&pos=full_text&logic=normal&exp_period=all_time&"
+hh_resume_url = "https://hh.ru/search/resume?&pos=full_text&logic=normal&exp_period=all_time"
+# https://hh.ru/search/resume?text=&pos=full_text&logic=normal&exp_period=all_time&page=
 
-async def async_query(session: aiohttp.ClientSession, headers: dict, url: str, query_params: dict, model: BaseModel):
-    
-    query_url = copy.deepcopy(url)
 
-    query_params = model.__dict__
-    for key, value in query_params.items():
-        query_url += f"&{str(key)}={str(value)}"
-    
-    return await async_request(session, headers, query_url)
-
-async def async_request(session: aiohttp.ClientSession, headers: dict, url: str):
-    async with session.get(headers=headers, url=url) as response:
-        return await response.json()
-
+# rotes
 @app.post("/api/hh/vacancies", response_model=HHVacanciesResponseModel)
 async def get_vacancies(model: HHVacanciesQueryParamsModel,
-                        user_agent: Annotated[str | None, Header()] = None):
+                        user_agent: Annotated[str | None, Header()] = None) -> dict:
     headers = {
         "User-Agent": user_agent
     }
 
     global hh_vacancies_url
-    query_url = copy.deepcopy(hh_vacancies_url)
-
-    query_params = model.__dict__
-    for key, value in query_params.items():
-        query_url += f"&{str(key)}={str(value)}"
-
     global aiohttp_session
-    data = await async_request(aiohttp_session, headers=headers, url=query_url)
+    data = await async_query(session = aiohttp_session, headers=headers, url=hh_vacancies_url, model=model)
     return data
 
-@app.post("/api/hh/resume", response_model=HHResumesResponseModel)
+@app.post("/api/hh/resume", response_model=HHResumesResponseModel   )
 async def get_resume(model: HHResumesQueryParamsModel,
-                     user_agent: Annotated[str | None, Header()] = None):
+                     user_agent: Annotated[str | None, Header()] = None) -> dict:
     headers = {
         "User-Agent": user_agent
     }
+
+    global hh_resume_url
+    global aiohttp_session
+    data = await async_query(session = aiohttp_session, headers=headers, url=hh_resume_url, model=model)
+    response = await get_amount_and_items(data=data)
+    return response
+
+
 
