e61275fcd0f8f24cc6c9235325fcc8d4b16104e5
lilvolcaninthenigthtime <alvicov@mail.ru>
2024-07-04 18:00:52 +0300

    volvo tha chto

--- src/backend/config/database.py
+++ src/backend/config/database.py
@@ -1,15 +1,20 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from ..models import Model
+from dotenv import load_dotenv
 from os import environ
+
+load_dotenv()
 
 DB_NAME = environ["DB_NAME"]
 DB_USER = environ["DB_USER"]
 DB_PASSWORD = environ["DB_PASSWORD"]
 DB_HOST = environ["DB_HOST"]
 DB_PORT = environ["DB_PORT"]
+DB_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/"
+print(DB_URL)
 
 engine = create_async_engine(
-    f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/"
+    DB_URL
 )
 new_session = async_sessionmaker(engine, expire_on_commit=False)
 
