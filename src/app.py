14e5316383109a07395e4c80f64307d49b64b6c8
lilvolcaninthenigthtime <alvicov@mail.ru>
2024-07-04 16:45:12 +0300

    docker dlya menya umer.

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 import aiohttp
 from .routers import hh_router
-from .database import create_tables, drop_tables
+from .config.database import create_tables, drop_tables
 
 
 aiohttp_clientsession: aiohttp.ClientSession = None
